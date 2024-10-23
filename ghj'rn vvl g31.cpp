9b751bcf38b755bf269ebc50c543536e244089dc
CallMeNezu <mosenkomihail49@gmail.com>
2024-10-23 17:22:28 +0700

    Add files via upload

---
+++ ghj'rn vvl g31.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+enum home_screen
+{
+    PLAY = 1,
+    QUIT = 2
+};
+
+int main()
+{
+    int player;
+    char pasword[15];
+
+    do {
+        std::cout << "Welcome to our project" << std::endl;
+        std::cout << "Play - 1" << std::endl;
+        std::cout << "quit - 2" << std::endl;
+        std::cin >> pasword;
+
+        switch (player){
+        case PLAY: {
+            //хубабуба мяумяумяу
+        }
+        case QUIT: {
+            break;
+        }
+        }
+
+
+    } while (player != 2);
+}
