87fe11522e9bb01ac7f0031b5d46fae3d826d3a9
CallMeNezu <mosenkomihail49@gmail.com>
2024-10-30 18:04:13 +0700

    Add files via upload

---
+++ buttons.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include "windows.h"
+void interface_buttons(int &a, int& b, int& c, int& d) {
+	bool click = false;
+    char plus_point_to_a_specific_stat;
+    while (true) {
+        if (GetAsyncKeyState('e')) //если кнопочка "е" нажата то открыть статистику
+        {
+            click = true;
+        }
+        else if (GetAsyncKeyState('r')) //если кнопочка "r" нажата то закрыть статистику 
+        {
+            click = false;
+        }
+        if (click == true) {
+            while (plus_point_to_a_specific_stat != 'q') {
+                std::cout << "enter 'h' to give a health point\n";
+                std::cout << "enter 'j' to give a strong point\n";
+                std::cout << "enter 'k' to give a charisma point\n";
+                std::cout << "enter 'l' to give a healing point\n";
+                std::cout << "press 'q' to exit\n";
+                std::cin >> plus_point_to_a_specific_stat;
+                if (plus_point_to_a_specific_stat == 'h') {
+
+                }
+            }
+        }
+	}
+}
---
+++ fghfdfgjuf.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include "windows.h"
+#include "buttons.h"//открыть статистику кнопочкой 
+enum home_screen
+{
+    PLAY = 112,
+    QUIT = 113,
+};
+struct stats {//статистика
+    char name[20];
+    int heal;
+    int strong;
+    int charisma;
+    int healing;
+};
+int main()
+{
+    interface_buttons();
+    char start;
+    do {
+        std::cout << "Welcome to our project" << std::endl;
+        std::cout << "Play - p" << std::endl;
+        std::cout << "quit - q" << std::endl;
+        std::cin >> start;
+        switch (start) {
+        case PLAY: {
+            std::cout << "background....\n";
+            Sleep(1000);
+            std::cout << ". ";
+            Sleep(1000);
+            std::cout << ". ";
+            Sleep(1000);
+            std::cout << ". \n";
+            Sleep(1000);
+            std::cout << "Damn, I forgot...\n";
+            Sleep(3000);
+            std::cout << "That's right! You fell into some kind of hole. \n";
+            Sleep(2000);
+            std::cout << "Get out of here.\n";
+            Sleep(750);
+            std::cout << "The pancakes are getting cold at home.\n";
+            break;
+        }
+        case QUIT: {
+            return 0;
+        }
+        default:
+            std::cout << "nope ";
+        }
+    } while (start != 'q' && start != 'p');//всё до этой строчки нач экран 
+
+    std::cout << "\nwhat is your name?\n";
+    stats player;
+    player.name;
+    player.heal = 100;
+    player.strong = 100;
+    player.charisma = 25;
+    player.healing = 25;
+    std::cin >> player.name;
+    int points = 99;
+
+}
